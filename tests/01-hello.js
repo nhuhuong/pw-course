4b660aadb8bf4911a40955c6f2fa33e98378b25e
scorpion <huong@wonolo.com>
2025-02-17 05:59:22 -0800

    lam bai tap

---
+++ tests/01-hello.js
@@ -0,0 +1,45 @@
+// console.log("Hello world")
+// var firstname  =" Huong"
+// let lastName ="Tran"
+
+// console.log(firstname + " " + lastName)
+
+// firstname = "mimi"
+// console.log(firstname + " " + lastName)
+
+// firstname =" Bap"
+
+// let name = "Huong"
+// let isLearningPlaywright = true
+// let price = 200
+// let str1 ="I'm ready"
+// console.log(str1)
+
+
+// const a = 10;
+// const b = 11 
+// const result = a > b 
+// const result2 = a === b 
+// const result3 = a > b 
+
+// console.log(result)
+
+// let i = 5 
+// console.log(i++)
+// console.log(i)
+
+
+// i=5
+// console.log(++i)
+// console.log(i)
+
+
+// if (5>7) {
+//     console.log ("dieu kien dung")
+// } else {
+//     console.log ("sai nha")
+// }
+
+for (let i = 1; i <= 10; i++) {
+    console.log("gia tri cua i la ", i);
+}
---
+++ tests/lession-2/ext4.js
@@ -0,0 +1,8 @@
+let chieucao = 1.75
+sole = Math.floor((chieucao * 100) % 100)
+cannanglituong = sole*9/10
+cannangtoida = sole
+cannangtoithieu = sole*8/10
+console.log("Bạn đã nhập:", chieucao);
+console.log("can nang li tuong la: " + cannanglituong + ", can nang toi da la: " + cannangtoida + ", can nang toi thieu la: " + cannangtoithieu)
+
