2ecc77159b1eca6dc4b74da4c316cb7428752714
scorpion <huong@wonolo.com>
2025-02-17 03:05:57 -0800

    test

---
+++ tests/lession-2/ext2.js
@@ -0,0 +1,5 @@
+myName = "Alex";
+myName = "Nagi";
+console.log(myName);
+
+// Note: Biến const không gán lại giá trị mới được
---
+++ tests/lession-2/ext3.js
@@ -0,0 +1,10 @@
+const new_number = 123
+if (new_number > 0) {
+    console.log("Gia tri ban nhap la so duong")
+}
+else if (new_number < 0) {
+    console.log("Gia tri ban nhap la so am")
+} 
+else {
+    console.log("Gia tri ban nhap la so khong")
+}
